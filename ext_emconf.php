1c5daf227c4b7cb749ec06c1d57c161b13450344
Marcin Sągol <m.sagol@macopedia.com>
2025-12-08 09:23:09 +0100

    [TASK] Add compatibility with TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,19 +4,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Headless Solr',
     'description' => 'This extension provides integration with solr to output content from TYPO3 in JSON format.',
     'state' => 'stable',
-    'author' => 'Łukasz Uznański',
+    'author' => 'Łukasz Uznański, Marcin Sągol',
     'author_email' => 'extensions@macopedia.com',
     'category' => 'fe',
     'internal' => '',
-    'version' => '3.0.0',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.99-12.9.99',
-            'frontend' => '11.0.99-12.9.99'
+            'typo3' => '13.4.0-13.4.99',
+            'frontend' => '13.4.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [
-            'headless' => '2.0.0-4.9.9'
+            'headless' => '4.0.0-4.9.9'
         ]
     ],
 ];
